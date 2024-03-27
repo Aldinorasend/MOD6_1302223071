45d6054bc10c44efb3c4b26f620c7a9e467547c6
unknown <aldinorasend@gmail.com>
2024-03-27 08:30:20 +0700

    penambahan design by contract pada semua kelas

--- MOD6/MOD6/Program.cs
+++ MOD6/MOD6/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Intrinsics.X86;
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.Intrinsics.X86;
 
 namespace MOD6
 {
@@ -11,12 +12,34 @@ namespace MOD6
             private String username;
             public SayaTubeUser(string username)
             {
-                this.username = username;
-                this.uploadedVideos = new List<SayaTubeVideo>();
-                Random rand = new Random();
-                int min = 10000;
-                int max = 99999;
-                this.id = rand.Next(min, max + 1);
+                int max = 100;
+                Contract.Requires(username.Length < max && username != null);
+                try
+                {
+                    checked
+                    {
+                        if ((username == null))
+                        {
+                            throw new ArgumentException("username tidak bisa kosong");
+                        }
+                        else if (username.Length > max)
+                        {
+                            throw new ArgumentException("username tidak bisa melebihi 200 karakter");
+                        }
+                        this.username = username;
+                        Random rand = new Random();
+                        int minRand = 10000;
+                        int maxRand = 99999;
+                        this.id = rand.Next(minRand, maxRand + 1);
+                        
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Error : {ex.Message}");
+                }
+
+                Contract.Ensures(this.username.Length < max && username != null);
 
             }
             public int getTotalVideoPlayCount()
@@ -31,7 +54,31 @@ namespace MOD6
 
             public void addVideo(SayaTubeVideo video)
             {
-                uploadedVideos.Add(video);
+                if (this.username != null)
+                {
+                    int max = 25000000;
+                    Contract.Requires(video != null);
+                    try
+                    {
+                        checked
+                        {
+                            if (video == null)
+                            {
+                                throw new OverflowException("Title Video tidak bisa null");
+                            } 
+                             uploadedVideos.Add(video);
+                            
+                        }
+                    }
+                    catch (OverflowException ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    Contract.Ensures(video != null);
+                   
+                } 
+               
+               
             }
 
             public void printAllVideoPlayCount()
@@ -52,12 +99,36 @@ namespace MOD6
             private int playCount;
 
             public SayaTubeVideo(String title) {
-                this.title = title;
-                Random rand = new Random();
-                int min = 10000;
-                int max = 99999;
-                this.id = rand.Next(min, max + 1);
-                this.playCount = 0;
+
+                int max = 100;
+                Contract.Requires(title.Length < max && title != null);
+                try
+                {
+                    checked
+                    {
+                        if ((title == null))
+                        {
+                            throw new ArgumentException("Title tidak bisa kosong");
+                        }
+                        else if (title.Length > max)
+                        {
+                            throw new ArgumentException("Title tidak bisa melebihi 200 karakter");
+                        }
+                        this.title = title;
+                        Random rand = new Random();
+                        int minRand = 10000;
+                        int maxRand = 99999;
+                        this.id = rand.Next(minRand, maxRand + 1);
+                        this.playCount = 0;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Error : {ex.Message}");
+                }
+
+                Contract.Ensures(this.title.Length < max && title != null);
+
             }
             public int getPlayCount()
             {
@@ -69,7 +140,27 @@ namespace MOD6
             }
             public void increasePlayCount(int playCount)
             {
-                this.playCount += playCount;
+                if (this.title != null)
+                {
+                    int max = 25000000;
+                    Contract.Requires(playCount < max);
+                    try
+                    {
+                        checked
+                        {
+                            if (this.playCount + playCount > max)
+                            {
+                                throw new OverflowException("Jumlah PlayCount melebihi 25.000.000");
+                            }
+                            this.playCount += playCount;
+                        }
+                    }
+                    catch (OverflowException ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    Contract.Ensures(this.playCount < max);
+                }
             }
             public void PrintVideoDetails()
             {
@@ -80,54 +171,63 @@ namespace MOD6
         }
         static void Main(string[] args)
         {
-            SayaTubeUser user1 = new SayaTubeUser("Aldi");
-            SayaTubeVideo vid1 = new SayaTubeVideo("Review Film 1 oleh Aldi" );
-            SayaTubeVideo vid2 = new SayaTubeVideo("Review Film 2 oleh Aldi");
-            SayaTubeVideo vid3 = new SayaTubeVideo("Review Film 3 oleh Aldi");
-            SayaTubeVideo vid4 = new SayaTubeVideo("Review Film 4 oleh Aldi");
-            SayaTubeVideo vid5 = new SayaTubeVideo("Review Film 5 oleh Aldi");
-            SayaTubeVideo vid6 = new SayaTubeVideo("Review Film 6 oleh Aldi");
-            SayaTubeVideo vid7 = new SayaTubeVideo("Review Film 7 oleh Aldi");
-            SayaTubeVideo vid8 = new SayaTubeVideo("Review Film 8 oleh Aldi");
-            SayaTubeVideo vid9 = new SayaTubeVideo("Review Film 9 oleh Aldi");
-            SayaTubeVideo vid10 = new SayaTubeVideo("Review Film 10 oleh Aldi");
+            try
+            {
+                SayaTubeUser user1 = new SayaTubeUser("Aldi");
+                SayaTubeVideo vid1 = new SayaTubeVideo("Review Film 1 oleh Aldi");
+                SayaTubeVideo vid2 = new SayaTubeVideo("Review Film 2 oleh Aldi");
+                SayaTubeVideo vid3 = new SayaTubeVideo("Review Film 3 oleh Aldi");
+                SayaTubeVideo vid4 = new SayaTubeVideo("Review Film 4 oleh Aldi");
+                SayaTubeVideo vid5 = new SayaTubeVideo("Review Film 5 oleh Aldi");
+                SayaTubeVideo vid6 = new SayaTubeVideo("Review Film 6 oleh Aldi");
+                SayaTubeVideo vid7 = new SayaTubeVideo("Review Film 7 oleh Aldi");
+                SayaTubeVideo vid8 = new SayaTubeVideo("Review Film 8 oleh Aldi");
+                SayaTubeVideo vid9 = new SayaTubeVideo("Review Film 9 oleh Aldi");
+                SayaTubeVideo vid10 = new SayaTubeVideo("Review Film 10 oleh Aldi");
 
-            user1.addVideo(vid1);
-            user1.addVideo(vid2);
-            user1.addVideo(vid3);
-            user1.addVideo(vid4);
-            user1.addVideo(vid5);
-            user1.addVideo(vid6);
-            user1.addVideo(vid7);
-            user1.addVideo(vid8);
-            user1.addVideo(vid9);
-            user1.addVideo(vid10);
+                user1.addVideo(vid1);
+                user1.addVideo(vid2);
+                user1.addVideo(vid3);
+                user1.addVideo(vid4);
+                user1.addVideo(vid5);
+                user1.addVideo(vid6);
+                user1.addVideo(vid7);
+                user1.addVideo(vid8);
+                user1.addVideo(vid9);
+                user1.addVideo(vid10);
+                vid1.increasePlayCount(1);
+                vid1.PrintVideoDetails();
+                vid2.increasePlayCount(1);
+                vid2.PrintVideoDetails();
+                vid3.increasePlayCount(1);
+                vid3.PrintVideoDetails();
+                vid4.increasePlayCount(1);
+                vid4.PrintVideoDetails();
+                vid5.increasePlayCount(1);
+                vid5.PrintVideoDetails();
+                vid6.increasePlayCount(1);
+                vid6.PrintVideoDetails();
+                vid7.increasePlayCount(1);
+                vid7.PrintVideoDetails();
+                vid8.increasePlayCount(1);
+                vid8.PrintVideoDetails();
+                vid9.increasePlayCount(1);
+                vid9.PrintVideoDetails();
+                vid10.increasePlayCount(1);
+                vid10.PrintVideoDetails();
+                Console.WriteLine(" ");
+                user1.printAllVideoPlayCount();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Argument Exception : " + ex.Message);
+                
+            }
+           
 
             
-           
-            vid1.increasePlayCount(1);
-            vid1.PrintVideoDetails();
-            vid2.increasePlayCount(1);
-            vid2.PrintVideoDetails();
-            vid3.increasePlayCount(1);
-            vid3.PrintVideoDetails(); 
-            vid4.increasePlayCount(1);
-            vid4.PrintVideoDetails();
-            vid5.increasePlayCount(1);
-            vid5.PrintVideoDetails();
-            vid6.increasePlayCount(1);
-            vid6.PrintVideoDetails();
-            vid7.increasePlayCount(1);
-            vid7.PrintVideoDetails();
-            vid8.increasePlayCount(1);
-            vid8.PrintVideoDetails();
-            vid9.increasePlayCount(1);
-            vid9.PrintVideoDetails();
-            vid10.increasePlayCount(1);
-            vid10.PrintVideoDetails();
-            Console.WriteLine(" ");
-            user1.printAllVideoPlayCount();
-           
+          
+        
             
 
         }
